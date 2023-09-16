6e695726156fc2a60f5737e49a1b9fbdf0120cfa
Alexander Grams <alexgrams222@gmail.com>
2023-09-15 20:28:28 -0700

    Exit and Options button functionality
    Exit quits the game, options button brings up blank basic menu.

---
+++ Esports Visual Novel/Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject optionsMenu;
+
+    /// <summary>
+    /// Closes the game. Also stops playing in editor if not running the built game.
+    /// </summary>
+    public void OnQuitGamePressed()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
