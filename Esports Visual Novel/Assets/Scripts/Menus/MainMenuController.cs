980e24a31c147d7437b707ff3e2d78b8b7a44adf
Alexander Grams <alexgrams222@gmail.com>
2023-09-18 15:36:37 -0700

    Transition from main menu to a new scene
    Pressing "New Game" button transitions to different scene.

--- Esports Visual Novel/Assets/Scripts/Menus/MainMenuController.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/MainMenuController.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    [SerializeField] private GameObject optionsMenu;
+    // How long it takes for the screen to fade out when switching scenes.
+    private const float FadeTime = 1.0f;
+
+    // Object that is used to fade out the screen.
+    [SerializeField] private CanvasGroup fadeCover;
+
+    void Start()
+    {
+        fadeCover.alpha = 0.0f;
+        fadeCover.gameObject.SetActive(false);
+    }
 
     /// <summary>
     /// Closes the game. Also stops playing in editor if not running the built game.
@@ -15,5 +26,26 @@ public class MainMenuController : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
         Application.Quit();
+    }
+
+    // Transition to a new scene by fading out the screen.
+    public void ChangeScene(string sceneName)
+    {
+        StartCoroutine(FadeOut(sceneName));
+    }
+
+    private IEnumerator FadeOut(string sceneName)
+    {
+        float currentFadeTime = 0.0f;
+
+        fadeCover.gameObject.SetActive(true);
+        while (currentFadeTime < FadeTime)
+        {
+            currentFadeTime += Time.deltaTime;
+            fadeCover.alpha = currentFadeTime / FadeTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 }
