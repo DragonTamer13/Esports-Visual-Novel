6c3cd3ca7979819da49c835832c5af9c30395731
Alexander Grams <alexgrams222@gmail.com>
2023-09-19 16:52:14 -0700

    Saving and loading using the menu
    Can save and load during the game using the SaveLoadMenu. Will add images and save game titles later. Next: different save slots.

---
+++ Esports Visual Novel/Assets/Scripts/Menus/SaveLoadButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+/// <summary>
+/// Controller script for a button in the Save and Load Game menu.
+/// Configure to save or load by calling SetIsSaving()
+/// </summary>
+public class SaveLoadButton : MonoBehaviour
+{
+    // TODO: Make this unique for each save slot button.
+    private const string saveDataKey = "Slot0";
+    private MainMenuController mainMenuController;
+    private bool isSaving; // True when button saves a game, false when button loads a game.
+
+    public void SetIsSaving(bool value)
+    {
+        isSaving = value;
+    }
+
+    void Start()
+    {
+        if (GameObject.Find("MainMenu") != null)
+        {
+            mainMenuController = GameObject.Find("MainMenu").GetComponent<MainMenuController>();
+        }
+    }
+
+    public void OnClick()
+    {
+        var saveManager = FungusManager.Instance.SaveManager;
+
+        // Save or load the game when this button is clicked
+        if (isSaving)
+        {
+            if (saveManager.NumSavePoints > 0)
+            {
+                saveManager.Save(saveDataKey);
+            }
+            print("Saved the game");
+        }
+        else
+        {
+            if (saveManager.SaveDataExists(saveDataKey))
+            {
+                // Do a fade transition if we're on the main menu. Otherwise, just load the game.
+                if (mainMenuController != null)
+                {
+                    mainMenuController.LoadGame(saveDataKey);
+                }
+                else
+                {
+                    saveManager.Load(saveDataKey);
+                    transform.parent.parent.gameObject.GetComponent<SaveLoadMenu>().Close();
+                }
+            }
+        }
+    }
+}
---
+++ Esports Visual Novel/Assets/Scripts/Menus/SaveLoadMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadMenu : MonoBehaviour
+{
+    // All SaveLoadButton components in the menu buttons.
+    private List<SaveLoadButton> buttons = new List<SaveLoadButton>();
+    private CanvasGroup canvasGroup; // Use a CG to show/hide this menu or else the Buttons' save/load state isn't
+                                     // set correctly the first time the menu is opened.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform t in transform.Find("SaveLoadButtons").transform)
+        {
+            buttons.Add(t.GetComponent<SaveLoadButton>());
+        }
+        canvasGroup = GetComponent<CanvasGroup>();
+        Close();
+    }
+
+    // Enable and show the menu.
+    private void Open()
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    /// <summary>
+    /// Open the menu to save game data.
+    /// </summary>
+    public void OpenSave()
+    {
+        foreach (SaveLoadButton b in buttons)
+        {
+            b.SetIsSaving(true);
+        }
+        Open();
+    }
+
+    /// <summary>
+    /// Open the menu to load game data.
+    /// </summary>
+    public void OpenLoad()
+    {
+        foreach (SaveLoadButton b in buttons)
+        {
+            b.SetIsSaving(false);
+        }
+        Open();
+    }
+
+    /// <summary>
+    /// Disable and hide the menu.
+    /// </summary>
+    public void Close()
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
