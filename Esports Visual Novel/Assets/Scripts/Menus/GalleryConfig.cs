2bb34a181ea741fc278437db9d558e0f0ee56b63
Libb24 <billwang1997@gmail.com>
2023-10-05 18:23:38 -0600

    Gallery in progress, Scrim results

---
+++ Esports Visual Novel/Assets/Scripts/Menus/GalleryConfig.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class GalleryConfig
+{/*
+    public static GalleryConfig activeConfig;
+    public const bool ENCRYPT = false;
+
+    public static string filePath => $"{FilePaths.root}gallery.vng";
+
+    public List<string> unlockedImages = new List<string>();
+
+    public static void Load()
+    {
+        if (File.Exists(filePath))
+        {
+            activeConfig = FileManager.Load<GalleryConfig>(filePath, encrypt: ENCRYPT);
+        }
+        else
+        {
+            activeConfig = new GalleryConfig();
+        }
+    }
+
+    public static void Save() => FileManager.Save(filePath, JsonUtility.ToJson(activeConfig), encrypt: ENCRYPT);
+
+    public static void Erase()
+    {
+        if (activeConfig == null)
+        {
+            activeConfig = new GalleryConfig();
+        }
+        activeConfig.unlockedImages = new List<string>();
+        Save();
+    }
+
+    public static void UnlockImage(string imageName)
+    {
+        if (activeConfig == null)
+        {
+            Load();
+        }
+
+        if (!activeConfig.unlockedImages.Contains(imageName))
+        {
+            activeConfig.unlockedImages.Add(imageName);
+            Save();
+        }
+    }
+
+    public static bool ImageIsUnlocked(string imageName)
+    {
+        if (activeConfig == null)
+        {
+            Load();
+        }
+
+        return activeConfig.unlockedImages.Contains(imageName);
+    }*/
+}
---
+++ Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrimResultsMenu : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvas;
+    [SerializeField] private GameObject[] panels;
+    private int curPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curPanel = 0;
+        panels[curPanel].SetActive(true);
+    }
+
+    public void next()
+    {
+        if (curPanel + 2 <= panels.Length)
+        {
+            panels[curPanel].SetActive(false);
+            curPanel++;
+            panels[curPanel].SetActive(true);
+        }
+    }
+}
