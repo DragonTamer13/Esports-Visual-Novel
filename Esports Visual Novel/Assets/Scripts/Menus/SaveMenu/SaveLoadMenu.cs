bb5b715f5a04cb9015f8e6f0e620fee4d63252ce
Alexander Grams <alexgrams222@gmail.com>
2023-10-11 17:05:32 -0700

    Outline and testing for cross-scene variables
    We need a way to set flowchart variables in one scene and get them in another scene.

--- Esports Visual Novel/Assets/Scripts/Menus/SaveMenu/SaveLoadMenu.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/SaveMenu/SaveLoadMenu.cs
@@ -119,6 +119,33 @@ public class SaveLoadMenu : MonoBehaviour
     /// <param name="saveLoadButton">The button that was clicked.</param>
     public void OnButtonClick(string saveDataKey, SaveLoadButton saveLoadButton)
     {
+        /// <summary>
+        /// TODO: More saving bullshit.
+        /// Premise: We will split the game into multiple scenes (one for each day), each with its own flowchart. This is so that 
+        /// multiple people can work on different days without issue. Also, it prevents having multiple flowcharts or one big 
+        /// flowchart in memory while playing the game.
+        /// 
+        /// Problem: Need a way for a flowchart to access the variables set in another flowchart in another scene.
+        /// 1. Can't put all the flowcharts in one scene because it conflicts with the premise and you can't access the variables of
+        ///    an inactive flowchart.
+        /// 2. Make a "datastore flowchart": Variables needed between flowcharts will all be stored in a flowchart with no nodes.
+        ///    We will make a bunch of public variables in this flowchart and make it a prefab so these changes apply across all scenes.
+        ///    a. Will need to change saving and loading so that we also save the information in the datastore each time we save/load.
+        ///       When a button is clicked to save, use Unity's native saving interface to save the datastore's state somehow. When
+        ///       loading, as soon as we get into the new scene, somehow tell the game which save slot was loaded and set the
+        ///       datastore's variables to the values saved in memory. Can be done easier if we can somehow call the "Save Point"
+        ///       and "OnSavepointLoaded" flowchart commands from another script.
+        /// 3. Use a Lua Store to save data
+        ///    a. Essentially the same as the datastore flowchart, except we then need an extra step to access data. Variables in the
+        ///       individual day flowcharts will be public. They will be set on load by copying from the datastore, which in turn has
+        ///       variables copied from the data stored on disk. More complicated than the datastore flowchart.
+        ///       
+        /// From this, despite its hackish appearance, using an empty flowchart prefab just to store public variables between scenes
+        /// is the best way to go. We now have these things to figure out:
+        /// 1. Find a way to save a second flowchart whenever we save the game.
+        /// 2. Tell a newly-loaded scene which save slot its associated with and which datastore to load.
+        /// 3. Set the variables of a flowchart from a script.
+        /// </summary>
         var saveManager = FungusManager.Instance.SaveManager;
 
         // Save or load the game when this button is clicked
