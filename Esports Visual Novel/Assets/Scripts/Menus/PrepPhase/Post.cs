5dd003674b432d2e3cc216d1e1e4597e9533d346
Alexander Grams <alexgrams222@gmail.com>
2023-10-30 17:57:25 -0700

    Set up social media posts by calling functions from flowchart
    Encountered problem with using variables in the message, trying to fix.

---
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Post : MonoBehaviour
+{
+    [SerializeField] private TMP_Text usernameText;
+    [SerializeField] private TMP_Text messageText;
+
+    // Set the text of this Post
+    public void SetText(string username, string text)
+    {
+        usernameText.text = username;
+        messageText.text = text;
+    }
+}
---
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/SocialMediaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SocialMediaController : MonoBehaviour
+{
+    // Parent of created Post UI objects.
+    [SerializeField] private GameObject postContainer;
+    [SerializeField] private GameObject postPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void CreatePost(string text)
+    {
+        int parseIndex = text.IndexOf(' ');
+
+        if (parseIndex < 0 || text.Length <= 0)
+        {
+            return;
+        }
+
+        GameObject newPost = Instantiate(postPrefab);
+
+        newPost.transform.SetParent(postContainer.transform);
+        newPost.transform.localScale = Vector3.one;
+        newPost.GetComponent<Post>().SetText(text.Substring(0, parseIndex), text.Substring(parseIndex + 1));
+    }
+}
