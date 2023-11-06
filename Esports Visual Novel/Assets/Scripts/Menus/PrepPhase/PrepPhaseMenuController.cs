601f1337274196e8f3102125655c37493fd6a253
Alexander Grams <alexgrams222@gmail.com>
2023-11-05 18:13:23 -0800

    Fixed bug where the SaveMenu UI would block some of the sub-UIs on the Prep Phase Menu

---
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/PrepPhaseMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrepPhaseMenuController : MonoBehaviour
+{
+    // The Save Menu UI in this scene.
+    [SerializeField] private GameObject saveMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (saveMenu == null)
+        {
+            saveMenu = GameObject.Find("SaveMenu");
+        }
+    }
+
+    public void ShowSaveMenu()
+    {
+        saveMenu.SetActive(true);
+    }
+
+    public void HideSaveMenu()
+    {
+        saveMenu.SetActive(false);
+    }
+}
