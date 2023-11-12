7e788864e9c0d7bb838d4186297e55fda1c5c4c0
Alexander Grams <alexgrams222@gmail.com>
2023-11-12 14:39:58 -0800

    Starting "About" menu
    Making a menu to explain core concepts of the game.

---
+++ Esports Visual Novel/Assets/Scripts/Menus/About/AboutMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Contains main functionality to swap between pages in the About Menu.
+/// </summary>
+public class AboutMenuController : MonoBehaviour
+{
+    // Parent object of the menu's page buttons.
+    [SerializeField] private Transform pageButtonHolder;
+
+    private List<Button> pageButtons = new List<Button>();
+    private Button currentButton = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject g in pageButtonHolder)
+        {
+            pageButtons.Add(g.GetComponent<Button>());
+        }
+    }
+
+    /// <summary>
+    /// Change the displayed page to match a clicked button.
+    /// </summary>
+    /// <param name="button">The button that was just clicked.</param>
+    public void SwitchToPage(Button button)
+    {
+        //pageButtons.FindIndex(b => b == button);
+        if (currentButton != null && currentButton != button)
+        {
+            currentButton.interactable = true;
+            currentButton = button;
+        }
+    }
+}
