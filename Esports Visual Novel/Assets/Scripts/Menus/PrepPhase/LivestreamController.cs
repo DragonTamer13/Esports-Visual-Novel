48c307da94a0a9d40472f5704ddf03a4f65ded99
Alexander Grams <alexgrams222@gmail.com>
2024-02-20 19:43:55 -0800

    Livestream message importing and parsing from CSV
    Added fonts to support special characters. Changed formatting for messages. NEXT: make emotes and fix new message creation formatting.

--- Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/LivestreamController.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/LivestreamController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -11,8 +13,8 @@ public class LivestreamController : MonoBehaviour
 {
     private readonly string[] Colors = { "red", "blue", "#008000", "#B22222", "#FF7F50", "#9ACD32", "#FF4500", "#2E8B57", 
                                          "#DAA520", "#D2691E", "#5F9EA0", "#1E90FF", "#FF69B4", "#8A2BE2", "#00FF7F" };
-    private readonly string[] Usernames = { "Name", "3749", "djskalfeh", "kjdahfDKJ", "89dddd2", "ThingThing", "IURnddkjlOI" };
-    private readonly string[] Messages = { "Hello world!", "How are you today?", "What a good match.", "I love this game!!!" };
+    //private readonly string[] Usernames = { "Name", "3749", "djskalfeh", "kjdahfDKJ", "89dddd2", "ThingThing", "IURnddkjlOI" };
+    //private readonly string[] Messages = { "Hello world!", "How are you today?", "What a good match.", "I love this game!!!" };
 
     // Parent object of chat messages
     [SerializeField] private Transform chat;
@@ -22,17 +24,82 @@ public class LivestreamController : MonoBehaviour
     private List<TMP_Text> chatMessages; 
     private float curTime = 0.0f;
     private int currentChatMessageIndex = 0;
+    private List<string> usernames = new List<string>();
+    private List<string> messages = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
         chatMessages = new List<TMP_Text>(chat.GetComponentsInChildren<TMP_Text>());
+
+        using (StreamReader reader = File.OpenText(Application.dataPath + "/Resources/LivestreamMessages.csv"))
+        {
+            int deliminator = 0;
+            string username = "";
+            string message = "";
+            string line = reader.ReadLine();
+            line = reader.ReadLine();
+
+            while (line != null)
+            {
+                deliminator = line.IndexOf(',');
+                if (deliminator >= 0)
+                {
+                    username = line.Substring(0, deliminator);
+                    message = line.Substring(deliminator + 1);
+                    if (username != "")
+                    {
+                        usernames.Add(username);
+                    }
+
+                    // Remove extra characters from the message while keeping its formatting.
+                    if (message.IndexOf('"') >= 0)
+                    {
+                        // The message is in quotes.
+                        int messageEnd = message.LastIndexOf("\"");
+                        if (messageEnd == message.Length - 1)
+                        {
+                            message = message.Substring(1, messageEnd - 1);
+                        }
+                        else
+                        {
+                            // The message spans multiple lines
+                            message = message.Substring(1);
+                            // TODO: improve condition
+                            while (line != null)
+                            {
+                                message += "\n";
+                                line = reader.ReadLine();
+                                messageEnd = line.LastIndexOf("\"");
+
+                                if (messageEnd >= 0)
+                                {
+                                    message += line.Substring(0, messageEnd-1);
+                                    break;
+                                }
+                                else
+                                {
+                                    message += line;
+                                }
+                            }
+                        }
+                    }
+
+                    if (message != "")
+                    {
+                        messages.Add(message);
+                    }
+                }
+
+                line = reader.ReadLine();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (curTime > 0.5f)
+        if (curTime > 0.25f)
         {
             CreateChatMessage();
             curTime = 0.0f;
@@ -49,8 +116,8 @@ public class LivestreamController : MonoBehaviour
     private void CreateChatMessage()
     {
         chatMessages[currentChatMessageIndex].text = "<color=" + Colors[Random.Range(0, Colors.Length)] + ">" + 
-                                                     Usernames[Random.Range(0, Usernames.Length)] + "</color>: " + 
-                                                     Messages[Random.Range(0, Messages.Length)];
+                                                     usernames[Random.Range(0, usernames.Count)] + "</color>: " + 
+                                                     messages[Random.Range(0, messages.Count)];
         chatMessages[currentChatMessageIndex].transform.SetSiblingIndex(chatMessages.Count - 1);
         currentChatMessageIndex = (currentChatMessageIndex + 1) % chatMessages.Count;
     }
