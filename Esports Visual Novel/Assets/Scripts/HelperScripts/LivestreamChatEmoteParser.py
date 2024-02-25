47c3499cbef2977e447204802d521269edc1adbd
Alexander Grams <alexgrams222@gmail.com>
2024-02-24 20:38:15 -0800

    Starting script to replace emote names in CSV with TMPro macro to create a custom image in text.

---
+++ Esports Visual Novel/Assets/Scripts/HelperScripts/LivestreamChatEmoteParser.py
@@ -0,0 +1,36 @@
+import csv;
+import os;
+
+ChatFileRelativePath = "\..\..\Resources\LivestreamMessages.csv"
+emotes = ("LUL")
+# <sprite="assetName" name="spriteName">
+
+if __name__ == "__main__":
+    print("Hello world")
+
+    dirname = os.path.dirname(__file__)
+    dirname = dirname.replace("Scripts\HelperScripts", "Resources\LivestreamMessages.csv")
+    write_dirname = dirname.replace("Messages", "Messages_TMP")
+    print(dirname)
+    headers = []
+    content = []
+
+    with open(dirname, newline='', encoding='utf8') as chatFile:
+        reader = csv.reader(chatFile, delimiter=",", quotechar='"')
+        headers = next(reader)
+        for row in reader:
+            new_message = ""
+            for word in row[1].split(" "):
+                if word in emotes:
+                    new_message += '<sprite="' + word + '" name="' + word + '"> '
+                else:
+                    new_message += word + " "
+
+            # -1 to trim the last space
+            content.append([row[0], new_message[:len(new_message)-1]])
+
+    with open(dirname, 'w', encoding='utf8', newline='') as write_csv:
+        writer = csv.writer(write_csv)
+
+        writer.writerow(headers)
+        writer.writerows(content)
