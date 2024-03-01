d8bd5aad3aeae978b3cda1855b5363022c739ecf
Alexander Grams <alexgrams222@gmail.com>
2024-02-29 21:59:38 -0800

    Planned out new ScrimResultsMenu for the new way that we're going to display scrim results information

--- Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScrimResultsMenu : MonoBehaviour
 {
+    private readonly string ScrimResultsCSVPath = "/Resources/LivestreamMessages.csv";
+
+    public enum MatchDay
+    {
+        Day1,
+        Day2
+    }
+
     [SerializeField] private GameObject[] panels;
     private int curPanel;
 
@@ -24,4 +33,33 @@ public class ScrimResultsMenu : MonoBehaviour
             panels[curPanel].SetActive(true);
         }
     }
+
+    /// <summary>
+    /// Set up the stats and comments for the players for a desired match day.
+    /// </summary>
+    /// <param name="matchDay">The day to display scrim results for.</param>
+    public void SetupScrimResults(MatchDay matchDay)
+    {
+        using (StreamReader reader = File.OpenText(Application.dataPath + ScrimResultsCSVPath))
+        {
+            int deliminator = 0;
+            string username = "";
+            string message = "";
+            string line = reader.ReadLine();
+            //line = reader.ReadLine();
+
+            while (line != null)
+            {
+                // TODO: Copy from LivestreamController.cs for reading and parsing the CSV.
+            }
+        }
+    }
+
+    /**
+     * Here's what's gonna happen:
+     * - New function here that takes in an enum. Each enum value corresponds to a match day. It uses the enum value to determine
+     *   which CSV to read in, then sets the values on the screen based on the CSV information.
+     * - A new Fungus command specifically sets up this menu for today. The only exposed variable is the day selector enum. 
+     *   When the command is run, call the above function with the value set in inspector.
+     */ 
 }
