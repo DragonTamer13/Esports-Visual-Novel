1a673f8863253b313fb86fce8fb8a95e888bd68b
Alexander Grams <alexgrams222@gmail.com>
2024-03-02 15:42:18 -0800

    Prep phase menu data is displayed from a CSV file

--- Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScrimResultsMenu : MonoBehaviour
 {
-    private readonly string ScrimResultsCSVPath = "/Resources/LivestreamMessages.csv";
+    private readonly string ScrimResultsCSVPath = "/Resources/ScrimResultsTest.csv";
 
     public enum MatchDay
     {
@@ -14,24 +15,20 @@ public class ScrimResultsMenu : MonoBehaviour
         Day2
     }
 
-    [SerializeField] private GameObject[] panels;
-    private int curPanel;
+    [SerializeField] private Image[] numberBackgrounds;
+    [SerializeField] private TextMeshProUGUI[] notes;
+
+    private List<TextMeshProUGUI> numberText = new List<TextMeshProUGUI>();
 
     // Start is called before the first frame update
     void Start()
     {
-        curPanel = 0;
-        panels[curPanel].SetActive(true);
-    }
-
-    public void next()
-    {
-        if (curPanel + 2 <= panels.Length)
+        // Each number background has a corresponding text element
+        foreach (Image i in numberBackgrounds)
         {
-            panels[curPanel].SetActive(false);
-            curPanel++;
-            panels[curPanel].SetActive(true);
+            numberText.Add(i.gameObject.GetComponentInChildren<TextMeshProUGUI>());
         }
+        SetupScrimResults(MatchDay.Day1);
     }
 
     /// <summary>
@@ -40,17 +37,51 @@ public class ScrimResultsMenu : MonoBehaviour
     /// <param name="matchDay">The day to display scrim results for.</param>
     public void SetupScrimResults(MatchDay matchDay)
     {
+
         using (StreamReader reader = File.OpenText(Application.dataPath + ScrimResultsCSVPath))
         {
-            int deliminator = 0;
-            string username = "";
-            string message = "";
             string line = reader.ReadLine();
-            //line = reader.ReadLine();
+            int numberCounter = 0;
+            int notesCounter = 0;
 
             while (line != null)
             {
-                // TODO: Copy from LivestreamController.cs for reading and parsing the CSV.
+                string[] parsedLine = line.Split(',');
+                for (int i = 0; i < parsedLine.Length; i++)
+                {
+                    if (i < parsedLine.Length-1)
+                    {
+                        numberText[numberCounter].text = parsedLine[i];
+                        switch (parsedLine[i])
+                        {
+                            case "1":
+                                numberBackgrounds[numberCounter].color = new Color(1.0f, 0.227f, 0.227f, 1.0f);
+                                break;
+                            case "2":
+                                numberBackgrounds[numberCounter].color = new Color(1.0f, 0.631f, 0.333f, 1.0f);
+                                break;
+                            case "3":
+                                numberBackgrounds[numberCounter].color = new Color(1.0f, 1.0f, 0.376f, 1.0f);
+                                break;
+                            case "4":
+                                numberBackgrounds[numberCounter].color = new Color(0.765f, 1.0f, 0.459f, 1.0f);
+                                break;
+                            case "5":
+                                numberBackgrounds[numberCounter].color = new Color(0.353f, 1.0f, 0.353f, 1.0f);
+                                break;
+                            default:
+                                numberBackgrounds[numberCounter].color = Color.white;
+                                break;
+                        }
+                        numberCounter++;
+                    }
+                    else
+                    {
+                        notes[notesCounter].text = parsedLine[i];
+                        notesCounter++;
+                    }
+                }
+                line = reader.ReadLine();
             }
         }
     }
