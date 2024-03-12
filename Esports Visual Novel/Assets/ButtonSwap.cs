5227174012a4832eb28c2ac57cacb10641c739e5
DragonTamer13 <wynekids@yahoo.com>
2024-03-12 16:58:47 -0700

    NarrativeLog Button Sprite Swap

---
+++ Esports Visual Novel/Assets/ButtonSwap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSwap : MonoBehaviour
+{
+    //Current Button Sprite
+    public GameObject button;
+
+    //Sprite to be used when the button is selected/pressed
+    public Sprite selectedSprite;
+
+    //Sprite to be used when the button is not selected/pressed
+    public Sprite deselectedSprite;
+
+    //Function to be called On Click() 
+    public virtual void ToggleButton()
+    {
+        if (button.GetComponent<Image>().sprite == selectedSprite) 
+        { 
+            button.GetComponent<Image>().sprite = deselectedSprite; 
+        }
+
+        else if (button.GetComponent<Image>().sprite == deselectedSprite) 
+        { 
+            button.GetComponent<Image>().sprite = selectedSprite; 
+        }
+    }
+}
