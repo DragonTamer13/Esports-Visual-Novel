cfe412983c6bc79e93cecd8118522acd925b1060
Alexander Grams <alexgrams222@gmail.com>
2024-03-19 17:51:50 -0700

    Deets menu likes and redeets set from CSV data
    Possible bug where the Deets messages overlap with each other, but wasn't able to reproduce on main game scenes. Might be fine?

--- Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/Post.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/Post.cs
@@ -7,11 +7,24 @@ public class Post : MonoBehaviour
 {
     [SerializeField] private TMP_Text usernameText;
     [SerializeField] private TMP_Text messageText;
+    [SerializeField] private TMP_Text likesText;
+    [SerializeField] private TMP_Text redeetsText;
 
     // Set the text of this Post
     public void SetText(string username, string text, string redeets = "0", string likes = "0")
     {
+        if (redeets == "")
+        {
+            redeets = "0";
+        }
+        if (likes == "")
+        {
+            likes = "0";
+        }
+
         usernameText.text = username;
         messageText.text = text;
+        likesText.text = likes;
+        redeetsText.text = redeets;
     }
 }
