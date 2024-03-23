b1f1f45e075394994fd14867f886fe16e1c75955
Alexander Grams <alexgrams222@gmail.com>
2024-03-23 15:43:42 -0700

    Scrim results controller updated for new layout

--- Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/ScrimResultsMenu.cs
@@ -9,21 +9,18 @@ public class ScrimResultsMenu : MonoBehaviour
 {
     private readonly string ScrimResultsCSVPath = "/ScrimResults.csv";
 
-    [SerializeField] private Image[] numberBackgrounds;
+    // Image components for displaying numerical stat values.
+    [SerializeField] private Image[] values;
+    // Text component for displaying notes.
     [SerializeField] private TextMeshProUGUI[] notes;
+    // Sprites for each possible numerical stat value, in decreasing order.
+    [SerializeField] private Sprite[] valueImages;
 
-    private List<TextMeshProUGUI> numberText = new List<TextMeshProUGUI>();
     private CanvasGroup canvasGroup;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Each number background has a corresponding text element
-        foreach (Image i in numberBackgrounds)
-        {
-            numberText.Add(i.gameObject.GetComponentInChildren<TextMeshProUGUI>());
-        }
-
         canvasGroup = GetComponent<CanvasGroup>();
         Hide();
     }
@@ -76,27 +73,22 @@ public class ScrimResultsMenu : MonoBehaviour
                     switch (line.Substring(i * 2, 1))
                     {
                         case "1":
-                            numberText[numberCounter].text = "1";
-                            numberBackgrounds[numberCounter].color = new Color(0.902f, 0.486f, 0.451f, 1.0f);
+                            values[numberCounter].sprite = valueImages[0];
                             break;
                         case "2":
-                            numberText[numberCounter].text = "2";
-                            numberBackgrounds[numberCounter].color = new Color(0.949f, 0.663f, 0.427f, 1.0f);
+                            values[numberCounter].sprite = valueImages[1];
                             break;
                         case "3":
-                            numberText[numberCounter].text = "3";
-                            numberBackgrounds[numberCounter].color = new Color(1.0f, 0.839f, 0.400f, 1.0f);
+                            values[numberCounter].sprite = valueImages[2];
                             break;
                         case "4":
-                            numberText[numberCounter].text = "4";
-                            numberBackgrounds[numberCounter].color = new Color(0.859f, 0.859f, 0.600f, 1.0f);
+                            values[numberCounter].sprite = valueImages[3];
                             break;
                         case "5":
-                            numberText[numberCounter].text = "5";
-                            numberBackgrounds[numberCounter].color = new Color(0.718f, 0.882f, 0.804f, 1.0f);
+                            values[numberCounter].sprite = valueImages[4];
                             break;
                         default:
-                            numberBackgrounds[numberCounter].color = Color.white;
+                            values[numberCounter].color = Color.white;
                             break;
                     }
                     numberCounter++;
