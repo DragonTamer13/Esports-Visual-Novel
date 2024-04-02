670d11817a5cb3e559283733009258bcaee620b3
Alexander Grams <alexgrams222@gmail.com>
2024-04-02 15:25:33 -0700

    Updated Deets and MinMax messages
    Fixed MinMax emote replacer script to work with the new livestream messages CSV formatting that we're using.

--- Esports Visual Novel/Assets/Scripts/HelperScripts/LivestreamChatEmoteParser.py
+++ Esports Visual Novel/Assets/Scripts/HelperScripts/LivestreamChatEmoteParser.py
@@ -5,29 +5,38 @@ ChatFileRelativePath = "\..\..\Resources\LivestreamMessages.csv"
 Emotes = ["LUL", "OMEGALUL", "4Head", "ABOBA", "POG", "POGGERS", "widepepOMEGAKEKHappyChampHands", "PeepoHands", "PoggieWoggie"]
 
 if __name__ == "__main__":
+    print("\nStarting script...")
     dirname = os.path.dirname(__file__)
-    dirname = dirname.replace("Scripts\HelperScripts", "Resources\LivestreamMessages.csv")
+    dirname = dirname.replace("Scripts\HelperScripts", "StreamingAssets")
     write_dirname = dirname.replace("Messages", "Messages_TMP")
-    print(dirname)
     headers = []
     content = []
+    
+    for filename in os.listdir(dirname):
+        if filename.startswith("MinMax_") and filename.endswith(".csv") and filename != "MinMax_Usernames.csv":
+            minmax_csv = dirname + "\\" + filename
+            with open(minmax_csv, newline='', encoding='utf8') as chatFile:
+                reader = csv.reader(chatFile, delimiter=",", quotechar='"')
+                headers = next(reader)
+                for row in reader:
+                    new_message = ""
+                    # Parse into words, then replace emote names with TMPro image macro
+                    for word in row[0].split(" "):
+                        if word in Emotes:
+                            new_message += '<sprite="' + word + '" name="' + word + '"> '
+                        else:
+                            new_message += word + " "
 
-    with open(dirname, newline='', encoding='utf8') as chatFile:
-        reader = csv.reader(chatFile, delimiter=",", quotechar='"')
-        headers = next(reader)
-        for row in reader:
-            new_message = ""
-            for word in row[1].split(" "):
-                if word in Emotes:
-                    new_message += '<sprite="' + word + '" name="' + word + '"> '
-                else:
-                    new_message += word + " "
+                    # -1 to trim the last space
+                    content.append([new_message[:len(new_message)-1]])
 
-            # -1 to trim the last space
-            content.append([row[0], new_message[:len(new_message)-1]])
+            # Write the file with replaced text
+            with open(minmax_csv, 'w', encoding='utf8', newline='') as write_csv:
+                writer = csv.writer(write_csv)
 
-    with open(dirname, 'w', encoding='utf8', newline='') as write_csv:
-        writer = csv.writer(write_csv)
+                writer.writerow(headers)
+                writer.writerows(content)
 
-        writer.writerow(headers)
-        writer.writerows(content)
+            print("Replaced emotes in", minmax_csv)
+
+    input("Press enter to quit.")
