8a87f450bdac0531638292dd4fc81d58546d2e9c
Alexander Grams <alexgrams222@gmail.com>
2024-04-07 16:15:52 -0700

    Ability to like and redeet posts on the social media menu

--- Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/Post.cs
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/Post.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Post : MonoBehaviour
@@ -9,6 +10,8 @@ public class Post : MonoBehaviour
     [SerializeField] private TMP_Text messageText;
     [SerializeField] private TMP_Text likesText;
     [SerializeField] private TMP_Text redeetsText;
+    [SerializeField] private GameObject likedImage;
+    [SerializeField] private GameObject redeetedImage;
 
     // Set the text of this Post
     public void SetText(string username, string text, string redeets = "0", string likes = "0")
@@ -26,5 +29,37 @@ public class Post : MonoBehaviour
         messageText.text = text;
         likesText.text = likes;
         redeetsText.text = redeets;
+    }
+
+    /// <summary>
+    /// Updates "Like" UI elements
+    /// </summary>
+    public void ToggleLike()
+    {
+        likedImage.SetActive(!likedImage.activeInHierarchy);
+        if (likedImage.activeInHierarchy)
+        {
+            likesText.text = (int.Parse(likesText.text) + 1).ToString();
+        }
+        else
+        {
+            likesText.text = (int.Parse(likesText.text) - 1).ToString();
+        }
+    }
+
+    /// <summary>
+    /// Updates "Redeet" UI elements
+    /// </summary>
+    public void ToggleRedeet()
+    {
+        redeetedImage.SetActive(!redeetedImage.activeInHierarchy);
+        if (redeetedImage.activeInHierarchy)
+        {
+            redeetsText.text = (int.Parse(redeetsText.text) + 1).ToString();
+        }
+        else
+        {
+            redeetsText.text = (int.Parse(redeetsText.text) - 1).ToString();
+        }
     }
 }
