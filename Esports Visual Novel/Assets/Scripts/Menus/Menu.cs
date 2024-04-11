2fb3b55a1b984522b62dd1364fa12fcb3aafa175
Alexander Grams <alexgrams222@gmail.com>
2024-04-10 22:38:34 -0700

    Basic review menu controller script
    Using inheritance like a normal person. Probably should have done this from the start.

---
+++ Esports Visual Novel/Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A UI windows that can be shown or hidden using a CanvasGroup component.
+/// </summary>
+public class Menu : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+        {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+        Hide();
+    }
+
+    /// <summary>
+    /// Make this menu visible and intertactable.
+    /// </summary>
+    public void Show()
+    {
+        canvasGroup.alpha = 1.0f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    /// <summary>
+    /// Make this menu invisible and unintertactable.
+    /// </summary>
+    public void Hide()
+    {
+        canvasGroup.alpha = 0.0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
---
+++ Esports Visual Novel/Assets/Scripts/Menus/PrepPhase/ReviewMenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main script for menu in the Prep Phase where the Coach can talk with the players.
+/// </summary>
+public class ReviewMenuController : Menu
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
