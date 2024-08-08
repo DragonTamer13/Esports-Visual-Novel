4ac032b0cd1e58d4b5364e1538c958416ab5d6ae
Alexander Grams <alexgrams222@gmail.com>
2024-08-07 20:47:28 -0700

    Dialog menu choice playing button sound

---
+++ Esports Visual Novel/Assets/Scripts/CustomFungusCommands/EVNMenuDialog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// EVN extension of default Fungus MenuDialog
+    /// </summary>
+    public class EVNMenuDialog : MenuDialog
+    {
+        // Sound made when a menu button is pressed
+        [SerializeField] private AudioClip buttonPressSound;
+
+        // Since the MenuDialog disables itself when a button is pressed, play the button click sound elsewhere.
+        public void PlayButtonClickSound()
+        {
+            FungusManager.Instance.MusicManager.PlaySound(buttonPressSound, FungusManager.Instance.MusicManager.GetSoundEffectVolume());
+        }
+    }
+}
