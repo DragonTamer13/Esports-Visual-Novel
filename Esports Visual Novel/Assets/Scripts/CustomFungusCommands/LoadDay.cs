66a560429e32992fb9a209b6cbc31b0e72224fc6
Alexander Grams <alexgrams222@gmail.com>
2024-08-18 15:40:10 -0700

    Added "Load Day" command to automatically handle moving DatastoreFlowchart variables to the next day
    Renamed Friday - 4 vs 3 nodes to not include the "vs 3" name of the opponent.

---
+++ Esports Visual Novel/Assets/Scripts/CustomFungusCommands/LoadDay.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Fungus
+{
+    /// <summary>
+    /// EVN-specific LoadScene command for transferring DatastoreFlowchart variables to the next scene.
+    /// </summary>
+    [CommandInfo("EVN",
+                 "Load Day",
+                 "For going from one VN scene to another. Uses a DatastoreLink to carry this day's DatastoreFlowchart variables to the next.")]
+    [AddComponentMenu("")]
+    [ExecuteInEditMode]
+    public class LoadDay : LoadScene
+    {
+        #region Public members
+
+        public override void OnEnter()
+        {
+            DatastoreLink datastoreLink = GameObject.Find("DatastoreLink").GetComponent<DatastoreLink>();
+            if (datastoreLink != null)
+            {
+                datastoreLink.LoadNextScene();
+            }
+            else
+            {
+                Debug.LogError("Called 'Load Day', but no DatastoreLink in this scene. Add a DatastoreLink prefab to this scene, or use the base 'Load Scene' command.");
+            }
+
+            base.OnEnter();
+        }
+
+        public override string GetSummary()
+        {
+            return base.GetSummary();
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(255, 204, 142, 255);
+        }
+
+        public override bool HasReference(Variable variable)
+        {
+            return base.HasReference(variable);
+        }
+
+        #endregion
+
+        #region Backwards compatibility
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+
+        #endregion
+    }
+}
