e48ec754f386697ae9ccedbbc3e39ec56baa4f61
Alexander Grams <alexgrams222@gmail.com>
2024-08-19 22:22:54 -0700

    Custom function to set some of a player's stats at the beginning of the prep phase

--- Esports Visual Novel/Assets/Scripts/CustomFungusCommands/SetPartialStats.cs
+++ Esports Visual Novel/Assets/Scripts/CustomFungusCommands/SetPartialStats.cs
@@ -11,22 +11,54 @@ namespace Fungus
     [ExecuteInEditMode]
     public class SetPartialStats : Command
     {
-        [Tooltip("Vector4 variable containing a player's stats")]
-        [SerializeField] protected Vector4Data StatsVector;
+        // Names of the main characters of EVN. Used as prefixes for character-specific variables.
+        public enum CharacterNames
+        {
+            Atropos,
+            Boiga,
+            Hajoon,
+            Maeday,
+            Velocity
+        }
 
-        [Tooltip("The player team's name")]
-        [SerializeField] protected StringData teamName;
+        [Tooltip("A player's new stat values. Enter 0 to leave stat unchanged.")]
+        [SerializeField] protected Vector4 newStats;
+
+        [Tooltip("The player whose stats are being modified")]
+        [SerializeField] protected CharacterNames characterName;
 
         #region Public members
         public override void OnEnter()
         {
+
+            Flowchart datastoreFlowchart = GameObject.Find("DatastoreFlowchart").GetComponent<Flowchart>();
+            Vector4 stats = datastoreFlowchart.GetVariable<Vector4Variable>(characterName.ToString() + "Stats").Value;
+
+            if (newStats.w >= 1)
+            {
+                stats.w = newStats.w;
+            }
+            if (newStats.x >= 1)
+            {
+                stats.x = newStats.x;
+            }
+            if (newStats.y >= 1)
+            {
+                stats.y = newStats.y;
+            }
+            if (newStats.z >= 1)
+            {
+                stats.z = newStats.z;
+            }
+
+            datastoreFlowchart.GetVariable<Vector4Variable>(characterName.ToString() + "Stats").Apply(SetOperator.Assign, stats);
 
             Continue();
         }
 
         public override string GetSummary()
         {
-            return "";
+            return characterName.ToString();
         }
 
         public override Color GetButtonColor()
